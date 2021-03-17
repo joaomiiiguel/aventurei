8ad980e12fc48466f97aa80999051817c9b0ce43
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-03-17 16:48:08 -0300

    Form Fale Conosco

--- src/components/paginainicial/ComoFunc/ComoFunc.js
+++ src/components/paginainicial/ComoFunc/ComoFunc.js
@@ -21,7 +21,7 @@ export default function ComoFunc() {
                     <p style={{ fontSize: '70%' }}>Passo 2</p>
                     <img src={imgPasso2} className="imgComoFunc" alt="Imagem de pesquisa" />
                     <h3 className="titleCardComo">Confira os detalhes do lugar</h3>
-                    <p>Dê uma olhada nas fotos, leia a descrição e as orientações do local de como chegar.</p>
+                    <p>Dê uma olhada nas fotos, leia a descrição e as orientações do local.</p>
                 </div>
                 <div className="CardComoFun">
                     <p style={{ fontSize: '70%' }}>Passo 3</p>
@@ -33,7 +33,7 @@ export default function ComoFunc() {
                     <p style={{ fontSize: '70%' }}>Passo 4</p>
                     <img src={imgPasso4} className="imgComoFunc" alt="Imagem de pesquisa" />
                     <h3 className="titleCardComo">Chegue e aventure-se!</h3>
-                    <p>Encontre-se com o Guia Aventurei no local e data marcado e depois é só aproveitar a aventura.</p>
+                    <p>Encontre-se com o Guia Aventurei na data e no local marcado e depois é só aproveitar a aventura.</p>
                 </div>
             </div>
         </div>
