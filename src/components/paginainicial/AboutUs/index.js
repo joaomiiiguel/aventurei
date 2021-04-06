365d8fd2313cdfc4701e01408cf11883fed490b6
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-04-06 15:54:02 -0300

    Add locais de Extreme

--- src/components/paginainicial/AboutUs/index.js
+++ src/components/paginainicial/AboutUs/index.js
@@ -7,7 +7,7 @@ export default function AboutUs() {
     return (
         <div className="container-About">
             <div className="logo-top"><img src={LogoBranco} alt="fogueira" className="logoImg" /></div>
-            <p className="textAbout">O Aventurei foi desenvolvido exclusivamente para compartilhar aventuras e proporcionar novas experiências. Nós acreditamos que a prática de atividades na natureza contribui para a qualidade de vida e para a ampliação da consciência ambiental. Por isso, estamos desenvolvendo tecnologias para aproximar os amantes da aventura e cultivar o estilo de vida outdoor.</p>
+            <p className="textAbout">O Aventurei foi desenvolvido para te proporcionar experiências marcantes. Nossa filosofia é cultivar a consciência ambiental, cuidando, preservando e melhorando a qualidade de vida dos nossos. Por este motivo, estamos desenvolvendo tecnologias para maximizar suas aventuras e equilibrar vivências incríveis, no melhor estilo de vida outdoor.</p>
         </div>
     )
 }
